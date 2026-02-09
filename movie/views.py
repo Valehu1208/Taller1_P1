8424b7350275d115e9de6002059dd370bbd50c1b
Valentina Hidalgo <vhidalgou@eafit.edu.co>
2026-02-08 23:26:24 -0500

    Search engine

--- movie/views.py
+++ movie/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 # Create your views here.
 def home(request):
-    #return HttpResponse('<h1>welcome to home page</h1>')
-    #return render(request, 'home.html')
-    return render(request, 'home.html', {'name':', valentina hidalgo'})
+    # búsqueda de películas
+    searchTerm = request.GET.get('searchMovie')
+
+    # si se está buscando una película
+    if searchTerm:
+        # lista únicamente la(s) película(s) cuyo título contiene el nombre buscado
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else: 
+        # lista todas las películas de la base de datos
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies': movies})
 
 def about(request):
     return render(request, 'about.html')
